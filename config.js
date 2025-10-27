74eecadf8fce1ccb9327b340e0fe7ee4c1cbc025
Prathamesh2801 <truepratham2801@gmail.com>
2025-10-27 10:40:21 +0530

    Arrival Json changes

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-export const BASE_URL = "https://tataalg.theeventpics.com/API";   // Economy 
-// export const BASE_URL = "https://tataa1g.theeventpics.com/API";  // Business
+// export const BASE_URL = "https://tataalg.theeventpics.com/API";   // Economy 
+export const BASE_URL = "https://tataa1g.theeventpics.com/API";  // Business
 // export const BASE_URL = "http://192.168.1.55/tataform/API";
 
 
 
-export const MODE = 'Economy'
-// export const MODE = 'Business'
+// export const MODE = 'Economy'
+export const MODE = 'Business'
